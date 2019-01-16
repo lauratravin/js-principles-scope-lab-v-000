862ff52bb60370f5190ef650dd1990efc64706e7
Laura Messa Travin <laura.travin@gmail.com>
2019-01-16 21:27:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(){
-   return  customerName.toUpperCase();
+  let str;
+   return str = customerName.toUpperCase();
 }
 
 function setBestCustomer(){

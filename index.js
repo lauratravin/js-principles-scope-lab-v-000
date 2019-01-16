9b5d4cf959609f34463916f224c62f39679a6bc4
Laura Messa Travin <laura.travin@gmail.com>
2019-01-16 21:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(){
-   return customerName = customerName.toUpperCase();
-}
+// function upperCaseCustomerName(){
+//    return customerName = customerName.toUpperCase();
+// }
+
+ function upperCaseCustomerName(){
+
+   return customerName = 'BOB';
+ }
+
 
 function setBestCustomer(){
     bestCustomer = 'not bob'  //has to be global scope

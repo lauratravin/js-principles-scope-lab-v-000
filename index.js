34d53455ff87288a0ca93edfc6947a793bb3543d
Laura Messa Travin <laura.travin@gmail.com>
2019-01-16 21:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-// function upperCaseCustomerName(){
-//    return customerName = customerName.toUpperCase();
-// }
+function upperCaseCustomerName(){
+   return customerName = customerName.toUpperCase();
+}
 
  function upperCaseCustomerName(){
 
